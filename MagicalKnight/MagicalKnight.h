e9a3bea4373fe3c99be94af975c5eea63228898d
ANNotunzdY® (Takayoshi Katsuki) <dev@annotunzdy.info>
2020-07-02 15:47:16 +0900

    Initial Commit

---
+++ MagicalKnight/MagicalKnight.h
@@ -0,0 +1,19 @@
+//
+//  MagicalKnight.h
+//  MagicalKnight
+//
+//  Created by あんのたん on 2020/07/02.
+//  Copyright © 2020 ANNotunzdY®. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagicalKnight.
+FOUNDATION_EXPORT double MagicalKnightVersionNumber;
+
+//! Project version string for MagicalKnight.
+FOUNDATION_EXPORT const unsigned char MagicalKnightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicalKnight/PublicHeader.h>
+
+
